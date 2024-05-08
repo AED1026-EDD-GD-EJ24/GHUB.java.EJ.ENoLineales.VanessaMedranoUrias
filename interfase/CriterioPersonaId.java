ec2e0c0a6b956dbcea6477af58e284938d999fa7
Vanessa Guadalupe Medrano Urías <147688736+VanessaMedranoUrias@users.noreply.github.com>
2024-05-08 16:39:32 +0000

    interfase criterio

--- interfase/CriterioPersonaId.java
+++ interfase/CriterioPersonaId.java
@@ -1,5 +1,8 @@
 package interfase;
 
 public class CriterioPersonaId extends Criterio<Persona> {
+    public int comparar(Persona a, Persona b){
+        return a.getId()-b.getId();
+    }
     
 }
--- interfase/CriterioPersonaNombre.java
+++ interfase/CriterioPersonaNombre.java
@@ -1,5 +1,8 @@
 package interfase;
 
 public class CriterioPersonaNombre extends Criterio <Persona>{
+    public int comparar(Persona a, Persona b){
+        return a.getNombre().compareTo(b.getNombre());
+    }
     
 }
