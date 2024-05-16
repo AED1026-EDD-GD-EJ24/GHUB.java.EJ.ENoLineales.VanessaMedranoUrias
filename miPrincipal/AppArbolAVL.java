6f2f1f0d2a448417ded0ec14c69d26944b92851a
Vanessa Guadalupe Medrano Urías <147688736+VanessaMedranoUrias@users.noreply.github.com>
2024-05-16 16:56:37 +0000

    actualizacion apparbolavl

--- miPrincipal/AppArbolAVL.java
+++ miPrincipal/AppArbolAVL.java
@@ -1,6 +1,14 @@
 package miPrincipal;
+import arbolAVL.*;
+import arbolAVL.Entero;
+
+import java.util.Scanner;
+import arbolBinarioOrdenado.*;
 public class AppArbolAVL{
     public static void menu(){
+        Scanner consola = new Scanner(System.in);
+        int opc =-1;
+        while(opc!=0){
         System.out.println("********************************");
         System.out.println("       ARBOLES AVL              ");
         System.out.println("********************************");
@@ -11,5 +19,44 @@ public class AppArbolAVL{
         System.err.println();
         System.out.println("0) SALIR                       ");
         System.out.print("Selecciona opción:");
+        
+        }
+        opc=consola.nextInt();
+        switch (opc) {
+            case 1:
+                insertarNodo();
+                break;
+            case 2:
+                buscarNodo();
+                break;
+            case 3:
+                eliminarNodos();
+                break;
+
+        
+            default:
+                break;
+        }
+    }
+    public static void insertarNodo(){
+        System.out.println("~~Insertar nodos ~~");
+        ArbolAVL arbolito = new ArbolAVL();
+        try {
+            //insertar algunos elementos 
+            arbolito.insertar(new Entero(2));
+            arbolito.insertar(new Entero(4));
+            arbolito.insertar(new Entero(6));
+          //  BTreePrinter.printNode(arbolito.getRaiz());
+          arbolito.getRaiz();
+          
+        } catch (Exception e) {
+           System.out.println("Error "+e.getMessage());
+        }
+    }
+    public static void eliminarNodos(){
+        System.out.println("~~Eliminar nodos ~~");
+    }
+    public static void buscarNodo(){
+        System.out.println("~~Buscar nodos ~~");
     }
 }
